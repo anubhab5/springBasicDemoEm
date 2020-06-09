de87175df539dba4a1ac192e804cff8b327050fe
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-06-09 11:01:37 +0530

    Added Rest Controller and Mapping

--- src/main/java/com/lerningspringboot/basicSpringBoot/controllers/MyTestController.java
+++ src/main/java/com/lerningspringboot/basicSpringBoot/controllers/MyTestController.java
@@ -1,10 +1,14 @@
 package com.lerningspringboot.basicSpringBoot.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyTestController {
 
+	@GetMapping("/hello")
 	public String myFirstSpringMethod() {
 		return "Hello Spring";
 	}
